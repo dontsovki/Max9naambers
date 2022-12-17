b7f3090f6f2f11879d64449b9e6234c811805184
Artem.don <dontsovki@gmail.com>
2022-12-17 14:34:19 +0300

    создали одну  задачи

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿int Max(int arq1, int arq2, int arq3);
+{ 
+    int result = arq1;
+    if (arq2 > result) result = arq2;
+    if (arq3 > result) result = arq3;
+    return result;
+}
+
+int a1 = 5;
+int b1 = 6;
+int c1 = 22;
+int a2 = 66;
+int c2 = 26;
+int b2 = 56;
+int a3 = 56;
+int c3 = 48;
+int b3 = 68;
+
+
+int max1 = Max(a1,c1,b1);
+int max2 = Max(a2,c2,b2);
+int max3 = Max(a3,c3,b3);
+int max = Max(max1,max2,max3);
+Console.WriteLine(max);
