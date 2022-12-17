632005466eae1eb52f8affd6a895a293664dcd08
Artem.don <dontsovki@gmail.com>
2022-12-17 14:59:20 +0300

    добавили массив

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,24 +1,24 @@
-﻿int Max(int arq1, int arq2, int arq3);
-{ 
+﻿int Max(int arq1, int arq2, int arq3)
+{
     int result = arq1;
     if (arq2 > result) result = arq2;
     if (arq3 > result) result = arq3;
     return result;
+
 }
 
-int a1 = 5;
-int b1 = 6;
-int c1 = 22;
-int a2 = 66;
-int c2 = 26;
-int b2 = 56;
-int a3 = 56;
-int c3 = 48;
-int b3 = 68;
+//              0.1.2.3.4.5.6.7.8.9
+int[] array = {2,26,85,45,69,20,45,62,48};
 
-
-int max1 = Max(a1,c1,b1);
-int max2 = Max(a2,c2,b2);
-int max3 = Max(a3,c3,b3);
-int max = Max(max1,max2,max3);
+int max = Max(
+    Max (array [0],array[1],array[2]),
+    Max (array[3],array[4],array[5]),
+    Max(array[6],array[7],array[8])
+);
 Console.WriteLine(max);
+
+//int max1 = Max(a1,c1,b1);
+//int max2 = Max(a2,c2,b2);
+//int max3 = Max(a3,c3,b3);
+//int max = Max(max1,max2,max3);
+//Console.WriteLine(max);
