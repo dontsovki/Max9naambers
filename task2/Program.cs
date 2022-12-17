5d8046aa6aaff83628f93c4937cc76a89f9a67ce
Artem.don <dontsovki@gmail.com>
2022-12-17 15:18:45 +0300

    добавили вторую задачу

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1,56,8,9,7,5,2,5,64,1205};
+int n = array.Length;
+int find = 18;
+ind index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
+    
+
