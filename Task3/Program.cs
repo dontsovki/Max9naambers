8f59069fc11d1865b1bad3dc874fe44f7e1a6be0
Artem.don <dontsovki@gmail.com>
2022-12-17 15:47:44 +0300

    добавили методы заполнения массива, распечатывание массива

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// методы заполнения массива, распечатывание массива
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        Index++;
+
+    }
+
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count);
+    {
+        Console.WriteLine(col[position]);
+        position++;
+
+    }
+}
+
+int[] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
